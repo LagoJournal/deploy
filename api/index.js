bf89e07aa1c109722ee52ea9b56461f104ccdbea
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-01-18 14:21:03 -0300

    index change

--- api/index.js
+++ api/index.js
@@ -25,10 +25,11 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log("%s listening at ", process.env.PORT); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log("%s listening at ", PORT); // eslint-disable-line no-console
   });
 });
